4ba2713e0fb88e1144f94b9192869798346f0ec3
Jonas Stenelind <jstenelind@gmail.com>
2024-04-17 09:27:38 +0200

    homepage

--- fabcollab/src/App.jsx
+++ fabcollab/src/App.jsx
@@ -1,11 +1,12 @@
 import './index.css'
 import './App.css'
+import HomePage from './pages/homepage/HomePage'
 
 function App() {
 
   return (
     <div className="app">
-      
+      <HomePage />
     </div>
   )
 }
