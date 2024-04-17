59511d2b7912e29c078890728a12be00118ad166
Adam Rehal <Niklasn89@gmail.com>
2024-04-17 09:11:26 +0200

    ändrat i dev

--- fabcollab/src/App.jsx
+++ fabcollab/src/App.jsx
@@ -1,11 +1,12 @@
 import './index.css'
 import './App.css'
+import SingleMoviePage from './pages/singlemoviepage/SingleMoviePage'
 
 function App() {
 
   return (
     <div className="app">
-      
+      <SingleMoviePage />
     </div>
   )
 }
