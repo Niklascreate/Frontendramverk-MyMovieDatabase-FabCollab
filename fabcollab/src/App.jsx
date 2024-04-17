ef57ff863789e0546148eeb587ae90cf210b35cf
Rindertanijk1997 <yhfe23.van.ri@folkuniversitetet.nu>
2024-04-17 10:08:00 +0200

    header nästan klar

--- fabcollab/src/App.jsx
+++ fabcollab/src/App.jsx
@@ -1,11 +1,14 @@
 import './index.css'
 import './App.css'
+import Header from './components/header/Header'
 
 function App() {
 
   return (
     <div className="app">
       
+      <Header/>
+
     </div>
   )
 }
